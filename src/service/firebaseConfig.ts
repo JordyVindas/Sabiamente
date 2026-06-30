d4779952cff18dc914f1f070c8fda4dcfc7b6aa7
Hieloxd <jordy.vindash@gmail.com>
2026-06-30 02:43:12 -0600

    Update firebaseConfig.ts
    a

--- src/service/firebaseConfig.ts
+++ src/service/firebaseConfig.ts
@@ -3,13 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxZBVwm2vQoIeeC2hPbdJ0EI_MLqVa9W4",
+  apiKey: "AIzaSyAmTjjEsqyjkk5Epedh2GyRMsPHx9etTV0",
   authDomain: "base-sabiamente.firebaseapp.com",
   projectId: "base-sabiamente",
   storageBucket: "base-sabiamente.firebasestorage.app",
   messagingSenderId: "705088946010",
   appId: "1:705088946010:web:1d500ba3c225f3181d7d63"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
